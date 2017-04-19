0f80643fd455a8a992afedf6f70a02be14fa6987
Satel Importadores <satelimportadores@gmail.com>
2017-04-19 14:20:02 -0500

    drag de mesas

--- js/index.js
+++ js/index.js
@@ -1,16 +1,33 @@
+var idmesa = 0;
+var i;
 $(document).ready(function() {
-	$( "td" ).draggable({ containment: "#containment-wrapper", scroll: false });
-	$( "#draggable" ).draggable();
+
 	$("td").droppable({
       drop: function( event, ui ) {
-        $( this )
-          .addClass( "ui-state-highlight" )
-          .find( "p" )
-            .html( "Dropped!" );
-      }
+          i = $( this ).find('img').attr('id');
+          alert(i);
+          if (!i) {
+            $( this ).html(ui.draggable); 
+            identificador_mesa = $( this ).find('img').attr('id');
+            columna = this.cellIndex;
+            fila = this.parentNode.rowIndex;
+            console.log(identificador_mesa+' '+fila+' '+columna);
+          };
+          
+        }
     });
+
 	 $("#rejilla").on("click", "td", function() {
-	 	alert('My position in table is: '+this.cellIndex+'x'+this.parentNode.rowIndex);
+	 	//console.log('My position in table is: '+this.cellIndex+'x'+this.parentNode.rowIndex);
      //alert($( this ).text());
    });
+
+   $("#addmesa").on('click',function() {
+    idmesa++;
+     $("#mesas").append('<img id="'+idmesa+'" class="draggable ui-widget-content" src="images/mesita01.png" alt="">');
+     $( ".draggable" ).draggable({ helper:'clone'});
+   });
 });
+
+
+
